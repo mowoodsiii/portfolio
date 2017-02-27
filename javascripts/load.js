4ea1e251b477f62ab94075dadadbd5633ba56e1c
Maurice Woods <mowoodsiii@gmail.com>
2017-02-27 10:27:22 -0600

    Content drop, adding descriptions to all prokjects

--- javascripts/load.js
+++ javascripts/load.js
@@ -2,6 +2,6 @@ function load(){
     planetarium();
     starfield();
     
-    var x = Math.floor((Math.random() * 3) + 1);
+    var x = Math.floor((Math.random() * 6) + 1);
     document.getElementById('profile').src = 'images/'+x+'.png';
 }
